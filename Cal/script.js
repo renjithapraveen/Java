06ea97eb067894673ac413c1c2ec7a14e54036a7
renjithapraveen <63199113+renjithapraveen@users.noreply.github.com>
2025-09-04 12:07:06 +0530

    Update script.js

--- Cal/script.js
+++ Cal/script.js
@@ -9,6 +9,29 @@ function setScreenValue(value){
 function calculateResult(){
     const resultElement = document.getElementById("result");
     const expression = resultElement.value.trim();
+
+
+if (expression === ""){
+    resultElement.value = "Enter an expression";
+    return;
+}            
+
+try{
+    resultElement.value = eval(expression);
+} catch(e){
+    resultElement.value = "Invalid Expression";
+}
+    function clearScreen(){
+    document.getElementById("result").value="";
+}
+
+function setScreenValue(value){
+    document.getElementById("result").value += value;
+}
+
+function calculateResult(){
+    const resultElement = document.getElementById("result");
+    const expression = resultElement.value.trim();
 }
 
 if (expression === ""){
@@ -20,4 +43,5 @@ try{
     resultElement.value = eval(expression);
 } catch(e){
     resultElement.value = "Invalid Expression";
+}
 }
